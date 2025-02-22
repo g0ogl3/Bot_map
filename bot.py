6def8b81d5d1ccac61eba58a118a7c2c67c4796a
mr. x1nii <152085667+g0ogl3@users.noreply.github.com>
2025-02-22 10:06:17 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from config import *
 from logic import *
 
 bot = telebot.TeleBot(TOKEN)
+user_marker_colors = {}
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
@@ -15,20 +16,31 @@ def handle_help(message):
                                       "/help - Список команд\n"
                                       "/show_city <город> - Показать город на карте\n"
                                       "/remember_city <город> - Запомнить город\n"
-                                      "/show_my_cities - Показать все запомненные города")
+                                      "/show_my_cities - Показать все запомненные города\n"
+                                      "/set_marker_color <цвет> - Установить цвет маркера, если он не будет установлен тогда цвет маркера будет красным")
 
 
 @bot.message_handler(commands=['show_city'])
 def handle_show_city(message):
-    city_name = message.text.split()[-1]
+    parts = message.text.split()
+    user_id = message.chat.id
+    city_name = parts[1]
+    marker_color = user_marker_colors[user_id] if user_id in user_marker_colors else 'red'
     coordinates = manager.get_coordinates(city_name)
     if coordinates:
         path = f"{city_name}.png"
-        manager.create_graph(path, [city_name])
+        manager.create_graph(path, [city_name], marker_color)
         with open(path, 'rb') as photo:
             bot.send_photo(message.chat.id, photo)
     else:
         bot.send_message(message.chat.id, 'Такого города я не знаю. Убедись, что он написан на английском!')
+
+@bot.message_handler(commands=['set_marker_color'])
+def handle_set_marker_color(message):
+    user_id = message.chat.id
+    color = message.text.split()[-1]
+    user_marker_colors[user_id] = color
+    bot.send_message(user_id, f'Цвет маркера установлен на {color}')
 
 
 @bot.message_handler(commands=['remember_city'])
@@ -42,15 +54,16 @@ def handle_remember_city(message):
 
 @bot.message_handler(commands=['show_my_cities'])
 def handle_show_visited_cities(message):
+    parts = message.text.split()
+    marker_color = parts[1] if len(parts) > 1 else 'red'
     cities = manager.select_cities(message.chat.id)
     if cities:
         path = "my_cities.png"
-        manager.create_graph(path, cities)
+        manager.create_graph(path, cities, marker_color)
         with open(path, 'rb') as photo:
             bot.send_photo(message.chat.id, photo)
     else:
         bot.send_message(message.chat.id, 'У вас нет сохраненных городов.')
-
 
 
 if __name__=="__main__":
